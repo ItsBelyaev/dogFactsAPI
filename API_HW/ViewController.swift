f1eee6755c69718bf97aae1cdafcdc9235e7ff52
down ultra <67225488+ultradown@users.noreply.github.com>
2021-08-06 18:00:38 +0300

    Initial Commit

---
+++ API_HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API_HW
+//
+//  Created by Daniil Belyaev on 06.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
