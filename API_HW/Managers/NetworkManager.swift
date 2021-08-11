f9cc9b161253489d5dd715fff6377eb1e65d9a24
down ultra <67225488+ultradown@users.noreply.github.com>
2021-08-11 10:14:08 +0300

    Alamofire

--- API_HW/Managers/NetworkManager.swift
+++ API_HW/Managers/NetworkManager.swift
@@ -6,32 +6,45 @@
 //
 
 import Foundation
+import Alamofire
 
 class NetworkManager {
     static let shared = NetworkManager()
         
+//    func fetchData(completionHandler: @escaping (Fact) -> Void) {
+//        let urlString = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all"
+//        guard let url = URL(string: urlString) else {return}
+//
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//            if let data = data {
+//                guard let facts = self.parseJSON(data: data) else {return}
+//                completionHandler(facts)
+//            }
+//        }.resume()
+//    }
+//    func parseJSON(data: Data) -> Fact? {
+//        let decoder = JSONDecoder()
+//        do {
+//            let decodedData = try decoder.decode(Fact.self, from: data)
+//            return decodedData
+//        } catch {
+//            print(error)
+//            return nil
+//        }
+//    }
+
     func fetchData(completionHandler: @escaping (Fact) -> Void) {
-        let urlString = "https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all"
-        guard let url = URL(string: urlString) else {return}
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data {
-                guard let facts = self.parseJSON(data: data) else {return}
-                completionHandler(facts)
-            }
-        }.resume()
-    }
-    func parseJSON(data: Data) -> Fact? {
-        let decoder = JSONDecoder()
-        do {
-            let decodedData = try decoder.decode(Fact.self, from: data)
-            return decodedData
-        } catch {
-            print(error)
-            return nil
+        AF.request("https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all", method: .get)
+            .validate().responseJSON { dataResponse in
+                switch dataResponse.result {
+                case .success(let value):
+                    let courses = FactElement.getFacts(from: value)
+                    completionHandler(courses)
+                case .failure(let error):
+                    print(error)
+                }
         }
     }
-    
     
     private init() {}
 }
--- API_HW/Models/Fact.swift
+++ API_HW/Models/Fact.swift
@@ -9,6 +9,17 @@ import Foundation
 
 struct FactElement: Codable {
     let fact: String
+    
+    static func getFacts(from value: Any) -> Fact {
+        guard let factsData = value as? [[String: Any]] else { return []}
+        var facts: [FactElement] = []
+        for factData in factsData {
+            let fact = FactElement(fact: factData["fact"] as! String)
+            facts.append(fact)
+        }
+        return facts as? Fact ?? []
+    }
+    
 }
 
 typealias Fact = [FactElement]
